fac53930156adccdc77c07168cabb33f192592b8
langnavina97 <81016364+langnavina97@users.noreply.github.com>
2022-05-09 22:46:52 -0300

    changed price oracle input param to pancakeswap router address

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -22,7 +22,7 @@ async function main() {
  
    await priceOracle.deployed();
 
-   priceOracle.initialize("0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd");
+   priceOracle.initialize("0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3");
 
    console.log("priceOracle deployed to:", priceOracle.address);
 
